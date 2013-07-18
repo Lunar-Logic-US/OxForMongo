c016f51cd1c5d472b1ba81bc8656c7ab05e4274e
Michael Calabrese <michael.calabrese@lunarlogic.com>
2013-07-18 10:44:20 -0700

    Updated Default route to a better version for general use.

--- app-blank/config/routes.php
+++ app-blank/config/routes.php
@@ -3,7 +3,9 @@
  * These are the routes that are specific to your application.
  */
 
+Ox_Router::add(WEB_ROOT, new Ox_FlatAction('root'));
+
 //Catches /root; /root/; /root/filename; /root/filename.php; /root/filename.html; /root/index.html
 Ox_Router::add('/^\/root\/?(\w+(\.(html|php))?)?$/', new Ox_FlatAction());
 // Catch all
-Ox_Router::add('/^\/(\w*)(\/\w*)?(\/\d*)?(\/\d*)?$/', new Ox_AssemblerAction(null, null, null));
+Ox_Router::add('/^\/(\w*)(\/\w*)?(\/[\w.\-_]*)?(\/[\w.\-_]*)?(\/[\w.\-_]*)?(\/\w*)?(\/\d*)?$/', new Ox_AssemblerAction());
