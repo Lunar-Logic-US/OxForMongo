520c801b62df179b5076127ec743f6fa1744d8fe
Michael Calabrese <michael.calabrese@lunarlogic.com>
2013-09-16 16:23:07 -0700

    Change to the partial download code to work correctly.

--- ox/lib/asset_managers/Ox_LocalAsset.php
+++ ox/lib/asset_managers/Ox_LocalAsset.php
@@ -80,6 +80,8 @@ class LocalAsset extends Ox_Asset
     public function getAsset($uri)
     {
         $db = Ox_LibraryLoader::Db();
+
+        //$uri = Ox_Router::trimPrefix($uri,Ox_Dispatch::CONFIG_WEB_BASE_NAME);
         $base_filename = pathinfo($uri, PATHINFO_FILENAME);
 
         $assets = $db->getCollection('assets');
@@ -94,48 +96,110 @@ class LocalAsset extends Ox_Asset
             header("HTTP/1.0 404 Not Found (File Missing)");
             exit(1);
         }
-
         $file_size = $asset['size'];
+
+        //-------------------------------------------------------------------------
+        //Process a partial file request from the client.
+        //-------------------------------------------------------------------------
 
         if (isset($_SERVER['HTTP_RANGE'])) {
             $partial_content = true;
-            $range = explode("-", $_SERVER['HTTP_RANGE']);
-            $offset = intval($range[0]);
-            $length = intval($range[1]) - $offset;
+            $range = $_SERVER['HTTP_RANGE'];
+            if (preg_match('/^bytes/',$_SERVER['HTTP_RANGE'])) {
+                //we have it in the form bytes=number-number
+                //We need to remove the bytes=
+                $split = explode('=',$range);
+                $range = $split[1];
+            }
+            $range = explode("-", $range);
+
+            if (strlen($range[0])==0 && isset($range[1]) ) {
+                // -1000
+                Ox_Logger::logDebug('LocalAsset::getAsset range[0] empty | [1] set: ');
+                $offsetLast = $file_size -1;
+                $offsetFirst = $file_size-intval($range[0]);
+                if ($offsetFirst<0) $offsetFirst=0;
+            } elseif (strlen($range[0])>0 && !isset($range[1])) {
+                // 1000-
+                Ox_Logger::logDebug('LocalAsset::getAsset range[0] NOT empty | [1] NOT set: ');
+                $offsetFirst = intval($range[0]);
+                $offsetLast = $file_size-1;
+            } else {
+                // 0-1000
+                Ox_Logger::logDebug('LocalAsset::getAsset GENERAL CASE: ');
+                $offsetFirst = intval($range[0]);
+                $offsetLast = intval($range[1]);
+            }
+
+            //Idiot check -- can not go base the end of the file.
+            if ($offsetLast > $file_size) {
+                $offsetLast = $file_size-1;
+            }
+
+            if($offsetFirst>$offsetLast){
+                //unsatisfiable range
+                header("Status: 416 Requested range not satisfiable");
+                header("Content-Range: */$file_size");
+                exit;
+            }
+
+            $length = $offsetLast - $offsetFirst+1; //Offsets start a 0, so need to +1 for length
+            $data_size = $length;
         }
         else {
             $partial_content = false;
-            $offset = 0;
+            $offsetFirst = 0;
             $length = $file_size;
-        }
-
-        //read the data from the file
-        $handle = fopen(DIR_UPLOAD . $base_filename, 'r');
-        $buffer = '';
-        fseek($handle, $offset);
-        $buffer = fread($handle, $length);
-        if ($partial_content) {
-            $data_size = intval($range[1]) - intval($range[0]);
-            $md5_sum = md5($buffer);
-        } else {
             $data_size = $file_size;
             $md5_sum = $asset['md5'];
         }
+
+        //-------------------------------------------------------------------------
+        //Get the data from the local file system
+        //-------------------------------------------------------------------------
+
+        //read the data from the file
+        $handle = fopen(DIR_UPLOAD . $base_filename, 'r');
+        if ($handle===false) {
+            Ox_Logger::logDebug('LocalAsset::getAsset Could not open the file: '. $base_filename);
+            header("HTTP/1.0 404 Not Found (Can not open file)");
+            exit(1);
+        }
+        $buffer = '';
+        $seekResult = fseek($handle, $offsetFirst);
+        if ($seekResult===-1) {
+            Ox_Logger::logDebug('LocalAsset::getAsset Could not seek the file: '. $base_filename . ' @ ' . $offsetFirst);
+            header("HTTP/1.0 404 Not Found (Can not seek in the file)");
+            exit(1);
+        }
+        $buffer = fread($handle, $length);
+        if ($partial_content) {
+            $md5_sum = md5($buffer);
+        }
         fclose($handle);
 
+        //-------------------------------------------------------------------------
+        //Start the file output
+        //-------------------------------------------------------------------------
+
+        //Make sure magic quotes are off.  Otherwise php will add unwanted slashes.
         @ini_set('magic_quotes_runtime', 0);
+
         // send the headers and data
+        if ($partial_content) {
+            header("HTTP/1.1 206 Partial content");
+            header('Content-Range: bytes ' . $offsetFirst . '-' . $offsetLast . '/' . $file_size);
+        }
         header("Content-Length: " . $data_size);
-        header("Content-md5: " . $md5_sum);
         header("Accept-Ranges: bytes");
-        if ($partial_content) header('Content-Range: bytes ' . $offset . '-' . ($offset + $length) . '/' . $file_size);
-        header("Connection: close");
-        //header('Content-Type: audio/mpeg');
         header('Content-Type: ' . $asset['type']);
-        //header('Content-Disposition: attachment; filename=' . $file_name);
         header('Content-Disposition: filename="' . $asset['original_name'] . '"');
+        header("Content-md5: " . $md5_sum);
         $fs = stat(DIR_UPLOAD . $base_filename);
-        header("Etag: ".sprintf('"%x-%x-%s"', $fs['ino'], $fs['size'],base_convert(str_pad($fs['mtime'],16,"0"),10,16)));
+        header("ETag: ".sprintf('"%x-%x-%s"', $fs['ino'], $fs['size'],base_convert(str_pad($fs['mtime'],16,"0"),10,16)));
+        header("Connection: close");
+
+        //Send the file data
         echo $buffer;
         flush();
         exit(0);
