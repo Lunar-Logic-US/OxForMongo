8e7c8af3fd19ba62255a0536d51c6c3f61303358
Michael Calabrese <michael.calabrese@lunarlogic.com>
2014-02-12 21:40:39 -0800

    Updated hook functions, and code reviewed them.

--- ox/lib/Ox_Hook.php
+++ ox/lib/Ox_Hook.php
@@ -24,6 +24,9 @@
  */
 class Ox_Hook
 {
+    const DEBUG = FALSE;
+    const SETTING_ID = 'Ox_Hook';
+
     /**
      * Singleton instance
      *
@@ -56,56 +59,69 @@ class Ox_Hook
      * Register a function on a hook.
      *
      * Example Hook:
-        "hooks"{
-            "menu":[
-                {
-                    "file":"/home/jesse/web/ivy/cart/hook.php",
-                    "class":"cartHook",
-                    "function":"scripts",
-                },
-                {
-                    "file":"/home/jesse/web/ivy/cart/hook.php",
-                    "class":"ordersHook",
-                    "function":"scripts",
-                }
-            ],
-            "orders":[
-                {
-                    "file":"/home/jesse/web/ivy/cart/Orders/hook.php",
-                    "class":"ordersHook",
-                    "function":"scripts",
-                }
-            ],
-            "home":[
-                {
-                    "file":"/home/jesse/web/ivy/site/hook.php",
-                    "class":"siteHook",
-                    "function":"sidebar",
-                }
-            ]
-        }
-     * 
+     * <code>
+     *  "hooks"{
+     *       "menu":[
+     * {
+     * "file":"/home/jesse/web/ivy/cart/hook.php",
+     * "class":"cartHook",
+     * "function":"scripts",
+     * },
+     * {
+     * "file":"/home/jesse/web/ivy/cart/hook.php",
+     * "class":"ordersHook",
+     * "function":"scripts",
+     * }
+     * ],
+     * "orders":[
+     * {
+     * "file":"/home/jesse/web/ivy/cart/Orders/hook.php",
+     * "class":"ordersHook",
+     * "function":"scripts",
+     * }
+     * ],
+     * "home":[
+     * {
+     * "file":"/home/jesse/web/ivy/site/hook.php",
+     * "class":"siteHook",
+     * "function":"sidebar",
+     * }
+     * ]
+     * }
+     *</code>
+     *
      * @param $name - The name of the hook, to be invoked in the execute function.
-     * @param $construct - The plugin where this hook is implemented
+     * @param $file
+     * @param $class
      * @param $function - The function to call in the above mentioned construct
      * @param bool $replace - Add an additional hook if false or replace the existing hook(s) if true.
+     * @internal param $construct - The plugin where this hook is implemented
      */
     public static function register($name, $file, $class, $function, $replace=false)
     {
+        if (self::DEBUG) Ox_Logger::logDebug(__CLASS__ .'-'. __FUNCTION__ .": $name - file: {$file} | class: {$class} | function: $function.");
         $update = array('$addToSet'=>array(
-            'hooks.'.$name=>array("file"=>$file, "class"=>$class, "function"=>$function)
+            $name=>array("file"=>$file, "class"=>$class, "function"=>$function)
         ));
-        if($replace){
+        /*
+        if ($replace) {
             $update = array('$set'=>array(
-                'hooks.'.$name=>array(array("file"=>$file, "class"=>$class, "function"=>$function))
+                $name=>array(array("file"=>$file, "class"=>$class, "function"=>$function))
             ));
         }
-        $db = Ox_LibraryLoader::db();
-        if(!empty($update)){
-            $db->settings->update(
-                array(),
-                $update
+        */
+        if (!empty($update)) {
+            if (self::DEBUG) Ox_Logger::logDebug(__CLASS__ .'-'. __FUNCTION__ .": Updating DB: " . print_r($update,1));
+            $db = Ox_LibraryLoader::Db();
+            //$status = $db->settings->insert(array('_id'=>'Ox_Hook',$name=>array()));
+
+            $status = $db->settings->update(
+                array('_id'=>self::SETTING_ID),
+                $update,
+                array('upsert'=>true)
             );
+
+            if (self::DEBUG) Ox_Logger::logDebug(__CLASS__ .'-'. __FUNCTION__ .": Update Status: " . print_r($status,1));
         }
         
     }
@@ -117,14 +133,16 @@ class Ox_Hook
      */
     public static function execute($name, $arguments=array())
     {
-        $db = Ox_LibraryLoader::db();
-        $settings = $db->settings->findOne();
+        if (self::DEBUG) Ox_Logger::logDebug(__CLASS__ .'-'. __FUNCTION__ .": Executing Hook: " .$name);
+        $db = Ox_LibraryLoader::Db();
+        $settings = $db->settings->findById(self::SETTING_ID );
         $output='';
-        if(isset($settings['hooks'][$name]) && is_array($settings['hooks'][$name])){
-            foreach($settings['hooks'][$name] as $hook){
+        if(isset($settings[$name]) && is_array($settings[$name])){
+            foreach($settings[$name] as $hook){
                 if(file_exists($hook['file'])){
                     require_once($hook['file']);
                     if(class_exists($hook['class'])){
+                        if (self::DEBUG) Ox_Logger::logDebug("Ox_Hook: Firing hook $name - file: {$hook['file']} | class: {$hook['function']} | name: $name.");
                         call_user_func_array(array($hook['class'],$hook['function']),$arguments);
                     } else {
                         Ox_Logger::logError("Hook class ({$hook['class']}) not found for $name.");
