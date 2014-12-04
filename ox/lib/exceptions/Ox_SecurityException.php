197c8ba86bbb977cd42a7b96f155c53a4a376b71
jbengtson <justinb541@gmail.com>
2014-12-03 16:20:28 -0800

    Additions for Hook and Security.

---
+++ ox/lib/exceptions/Ox_SecurityException.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *    Copyright (c) 2012 Lunar Logic LLC
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 2012 Lunar Logic LLC
+ * @license http://www.gnu.org/licenses/agpl-3.0.html AGPLv3
+ * @package Ox_Exceptions
+ */
+
+/**
+ * Exception for security issues.
+ * @package Ox_Exceptions
+ */
+class Ox_SecurityException extends Ox_Exception {
+    /**
+     * String to prepend on the error message for the log.
+     * @var string
+     */
+    protected static $_logHeader='Ox_SecurityException:';
+}
