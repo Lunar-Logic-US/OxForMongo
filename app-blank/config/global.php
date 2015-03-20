3f31e4fb333a3954f638245c0351205cd49ecd13
Michael Calabrese <michael.calabrese@lunarlogic.com>
2015-03-20 08:52:03 -0700

    Basic reorganization changes.  Removed blank-app. The root of the project is the main directory.

--- app-blank/config/global.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * This is the place to put global requires/defines for your application.  This file
- * is included before dispatch is loaded and run.
- */
--- app-blank/config/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * These are the routes that are specific to your application.
- */
-
-// Catch all
-Ox_Router::add('/^\/(\w*)(\/\w*)?(\/[\w.\-_]*)?(\/[\w.\-_]*)?(\/[\w.\-_]*)?(\/\w*)?(\/\d*)?$/', new Ox_AssemblerAction());
