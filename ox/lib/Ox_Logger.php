006bbddd040d900460286721ee72eded76d7d4be
lunar-ren <rendahl.weishar@lunarlogic.com>
2015-07-07 17:29:22 -0700

    Updating logger to respect log levels

--- ox/lib/Ox_Logger.php
+++ ox/lib/Ox_Logger.php
@@ -13,7 +13,14 @@
  *
  *    You should have received a copy of the GNU Affero General Public License
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 2012 Lunar Logic LLC
+ * @license http://www.gnu.org/licenses/agpl-3.0.html AGPLv3
+ * @package Ox_Logging
  */
+
+//Making this available for all user code
+require_once(OX_FRAMEINTERFACE  . 'Ox_LogWriter.php');
 
 /**
  * Class to facilitate logging.
@@ -21,61 +28,58 @@
  * There are three log levels available: MESSAGE, WARNING, ERROR and DEBUG. The
  * logger can be configured to react to one, all, or any combination of the
  * available logging levels.
- *
+ * <br><br>
  * In addition, one, or many logging levels can be associated with a specific
  * logger that implements the 'Logger' interface. The default is to set all
  * to 'FileLogger'.
- *
+ * <br><br>
  * This class is setup to currently run both as a pseudo singleton and a regular class.  This was
- * done for backward compatibility
- *
+ * done for backward compatibility.
+ * <br><br>
+ * Do log in your code:<pre><code>
+ * Ox_Logger::logDebug("Server URI: " .$_SERVER['REQUEST_URI']); //just a plain debug message
+ * Ox_Logger::logDebug($_SERVER); //print_r the variable into the log
+ * Ox_Logger::logError("Error in Request URI: " .$_SERVER['REQUEST_URI']); //log an error message
+ * </code></pre>
  * TODO: Add config setting to set which logWriter is going to be used.
+ * @package Ox_Logging
  */
-
-//search replace: $this->logger->log with Ox_Logger::log
-//search replace: $logger->log with Ox_Logger::log
-
-//Making this available for all user code
-require_once(OX_FRAMEINTERFACE  . 'Ox_LogWriter.php');
-
 class Ox_Logger
 {
+    //These are bit flags and types for setting what is written to the log
+    /** Log level None - Nothing is logged */
     const NONE    = 0;
+    /** Log level Message */
     const MESSAGE = 1;
+    /** Log level Warning */
     const WARNING = 2;
+    /** Log Level Error */
     const ERROR   = 4;
+    /** Log Level Debug */
     const DEBUG   = 8;
+
+    /** Default name for the ox log */
     const DEFAULT_FILENAME = 'ox.log';
+
+    /** Default log writer object */
     const LOG_WRITER = 'Ox_FileLogWriter';
 
-    /**
-     * Specify the directory of the log writers
-     */
+    /** Directory of the log writers */
     private static $_lib;
     
-    /**
-     * Specifies the level of the entry (e.g. notice, warning, error) see above.
-     */
+    /** Specifies the level of the entry (e.g. notice, warning, error) see above. */
     private static $_logLevel;
     
-    /**
-     * The format of to use for the timestamp.
-     */
+    /** Format of the timestamp. */
     private static $datetime_format = "\[d\/M\/Y:H:i:s\]";
     
-    /**
-     * An include path.
-     */
+    /** An include path. */
     private static $_classPath;
 
-    /**
-     * Track usage by marking initialization flag.
-     */
+    /** Track usage by marking initialization flag. */
     private static $_initialized=FALSE;
     
-    /**
-     * Error level handlers.
-     */
+    /** Error level handlers. */
     private static $_handlers = array(
         Ox_Logger::MESSAGE => null,
         Ox_Logger::WARNING => null,
@@ -84,9 +88,11 @@ class Ox_Logger
     );
 
     /**
-     * Constructor initializes the logger using optionally passed in parameters.
+     * Constructor.
      *
-     * @param String $log_file
+     * Initializes the logger using optionally passed in parameters.
+     *
+     * @param string $log_file
      * @param int $level
      */
     public function __construct($log_file=null, $level = 0)
@@ -110,14 +116,14 @@ class Ox_Logger
      * This can be run at any time that we need to send a message.  This means that you can just log a message
      * and this object will initialize itself.
      *
-     * @param null $logFile Name of the log file
-     * @param null $level
+     * @param null|string $logFile Name of the log file
+     * @param int $level
      * @return bool
      * @throws Exception
      */
     private static function _init($logFile=null, $level = null) {
         if (!self::$_initialized) {
-            global $config_parser;
+            $config_parser = Ox_LibraryLoader::Config_Parser();
             if (!isset($config_parser) && $logFile===null) {
                 return false;
             }
@@ -125,7 +131,6 @@ class Ox_Logger
             self::$_classPath = array(self::$_lib,DIR_APPLIB);
 
             self::$_initialized = TRUE;
-            global $config_parser;
             if (empty($logFile)) {
                 $logPath = $config_parser->getAppConfigValue('log_dir');
                 if (empty($logPath)) {
@@ -144,7 +149,13 @@ class Ox_Logger
                 $logFile = $logPath . $logName;
             }
             if ($level === null) {
-                $level = (int) $config_parser->getAppConfigValue('log_level');
+                $level = $config_parser->getAppConfigValue('log_level');
+                // No log level set? Default to all levels
+                if($level === null) {
+                    $level = self::allLogLevels();
+                } else {
+                    $level = (int) $level;
+                }
             }
             self::$_logLevel = $level;
             $writer = self::LOG_WRITER;
@@ -157,10 +168,10 @@ class Ox_Logger
     }
 
     /**
-     * Set the logger for a log message type
+     * Set the logger for a log message type.
      *
-     * @param $type
-     * @param $logger
+     * @param int $type
+     * @param Ox_LogWriter $logger
      */
     public static function setLogger($type, $logger)
     {
@@ -168,8 +179,8 @@ class Ox_Logger
     }
 
     /**
-     * Set the current log level
-     * @param $level
+     * Set the current log level.
+     * @param int $level
      */
     public static function setLevel($level)
     {
@@ -179,21 +190,21 @@ class Ox_Logger
     /**
      * Send the message to the correct logWriter
      *
-     * @param $level
-     * @param $p_message
+     * @param int $level
+     * @param string $p_message
      */
     private static function _logIt($level, $p_message)
     {
         //only run if we are initialized
         if (self::_init()) {
             $message = print_r( $p_message, 1 );
-            if($level == Ox_Logger::MESSAGE) {
+            if(($level & self::$_logLevel) == Ox_Logger::MESSAGE) {
                 self::$_handlers[Ox_Logger::MESSAGE]->log(self::_getFormattedTimestamp().' MESSAGE '.$message);
-            } elseif($level == Ox_Logger::WARNING) {
+            } elseif(($level & self::$_logLevel) == Ox_Logger::WARNING) {
                 self::$_handlers[Ox_Logger::WARNING]->log(self::_getFormattedTimestamp().' WARNING '.$message);
-            } elseif($level == Ox_Logger::ERROR) {
+            } elseif(($level & self::$_logLevel) == Ox_Logger::ERROR) {
                 self::$_handlers[Ox_Logger::ERROR]->log(self::_getFormattedTimestamp().' ERROR '.$message);
-            } elseif($level == Ox_Logger::DEBUG) {
+            } elseif(($level & self::$_logLevel) == Ox_Logger::DEBUG) {
                 self::$_handlers[Ox_Logger::DEBUG]->log(self::_getFormattedTimestamp().' DEBUG '.$message);
             }
         }
@@ -201,8 +212,8 @@ class Ox_Logger
     
     // Wrapper functions for the 4 log levels
     /**
-     * Send a message of type MESSAGE
-     * @param $message
+     * Send a message of type MESSAGE.
+     * @param string $message
      */
     public static function logMessage($message)
     {
@@ -211,7 +222,7 @@ class Ox_Logger
 
     /**
      * Send a message of type WARNING
-     * @param $message
+     * @param string $message
      */
     public static function logWarning($message)
     {
@@ -220,7 +231,7 @@ class Ox_Logger
 
     /**
      * Send a message of type ERROR
-     * @param $message
+     * @param string $message
      */
     public static function logError($message)
     {
@@ -229,7 +240,7 @@ class Ox_Logger
 
     /**
      * Send a message of type DEBUG
-     * @param $message
+     * @param string $message
      */
     public static function logDebug($message)
     {
